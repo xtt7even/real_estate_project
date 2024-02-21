a640fcac7b7c2dfdc5d1668d423fb4d8c857aa5e
Kirill <42815480+EloTheWizzard@users.noreply.github.com>
2024-02-21 16:54:11 +0100

    Big Listing styling complete

--- src/components/Listings/FullScreenListings/ListingBig.js
+++ src/components/Listings/FullScreenListings/ListingBig.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./ListingBig.css"
 
+import mapPin from "../../../images/icons/maps-pin.png"
+import homeIcon from "../../../images/icons/home-icon.png"
+import rulerIcon from "../../../images/icons/ruler-icon.png"
+import bathroomIcon from "../../../images/icons/sink-wash-basin-icon.png"
+import bedroomIcon from "../../../images/icons/sleep-icon.png"
+import fileIcon from "../../../images/icons/file-black-icon.png"
+
+import photoChangeArrow from "../../../images/icons/thin-chevron-round-left-icon.png"
+
+import closeIcon from "../../../images/icons/close-line-icon.png"
+
 function ListingBig(props)
 {
     const formatter = new Intl.NumberFormat('en-US', {
@@ -14,14 +25,16 @@ function ListingBig(props)
     return (
         <div id="ListingBig--container--background">
             <div id="ListingBig--container">
-                <button onClick={() => props.handleCloseBigListing()}>Close</button>
+                <button onClick={() => props.handleCloseBigListing()} id="ListingBig--closebtn">
+                    <img src={closeIcon}></img>
+                </button>
                 <div id="ListingBig--photocontainer">
                             <div className="ListingBig--photo--shadows" id="left">
-                            
+                                <img src={photoChangeArrow}></img>
                             </div>
                             <img id="ListingBig--activephoto" src={props.property.photos[0]}></img> {/* hardcoded for now, should be dynamic */}
                             <div className="ListingBig--photo--shadows" id="right">
-        
+                                <img src={photoChangeArrow}></img>
                             </div>
                     
                 </div>
@@ -31,13 +44,44 @@ function ListingBig(props)
                     <h3 id='ListingBig--price'>{formattedPrice}</h3>
                     
                     <div id="ListingBig--features--container">
-                        <div className="ListingBig--featurebox"></div>
-                        <div className="ListingBig--featurebox"></div>
-                        <div className="ListingBig--featurebox"></div>
-                        <div className="ListingBig--featurebox"></div>
-                        <div className="ListingBig--featurebox"></div>
-                        <div className="ListingBig--featurebox"></div>
+                        <div className="ListingBig--featurebox">
+                            <img src={mapPin}></img>
+                            <div className="Feature--content">
+                                <p>{props.property.location}</p>
+                            </div>
+                        </div>
+                        <div className="ListingBig--featurebox">
+                            <img src={bedroomIcon}></img>
+                            <div className="Feature--content">
+                                <p>Bedrooms: {props.property.numOfBedrooms}</p>
+                            </div>
+                        </div>
+                        <div className="ListingBig--featurebox">
+                            <img src={bathroomIcon}></img>
+                            <div className="Feature--content">
+                                <p>Bathrooms: {props.property.numOfBathrooms}</p>
+                            </div>
+                        </div>
+                        <div className="ListingBig--featurebox">
+                            <img src={rulerIcon}></img>
+                            <div className="Feature--content">
+                                <p>Area: {props.property.area}sqm</p>
+                            </div>
+                        </div>
+                        <div className="ListingBig--featurebox">
+                            <img src={fileIcon}></img>
+                            <div className="Feature--content">
+                                <p>Condition: {props.property.condition}</p>
+                            </div>
+                        </div>
+                        <div className="ListingBig--featurebox">
+                            <img src={homeIcon}></img>
+                            <div className="Feature--content">
+                                <p>Property Type: {props.property.propertyType}</p>
+                            </div>
+                        </div>
                     </div>
+
 
                     <div id="ListingBig--desription--container">
                         <h4>Desription</h4>
@@ -47,15 +91,20 @@ function ListingBig(props)
                     </div>
                 </div>
 
-                {/* <h4>Interested in this property? Contact Us!</h4> */}
+                
                 <div id="ListingBig--form--container">
+                    <h4>Interested in this property? Contact Us!</h4>
+                    <div id="ListingBig--form--background">
+                        <form>
+                            <input type="text" className="Form--input--small" placeholder="Full Name" name="name"></input>
+                            <input type="text" className="Form--input--small" placeholder="E-Mail" name="email"></input>
+                            <input type="text" className="Form--input--small" placeholder="Phone Number" name="phonenumber"></input>
+                            <input type="text" className="Form--input--big" placeholder="Message" name="message"></input>
+                            <button id="Form--sendbtn">Send</button>
+                        </form>
+                    </div>
+
                     
-                    <form>
-                        <input type="text" className="Form--input--small" placeholder="Full Name" name="name"></input>
-                        <input type="text" className="Form--input--small" placeholder="E-Mail" name="email"></input>
-                        <input type="text" className="Form--input--small" placeholder="Phone Number" name="phonenumber"></input>
-                        <input type="text" className="Form--input--big" placeholder="Message" name="message"></input>
-                    </form>
                 </div>
                 
             </div>
