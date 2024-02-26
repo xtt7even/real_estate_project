4bba8bd9444752751a9e931b2521f4a06b099e6a
Kirill <42815480+EloTheWizzard@users.noreply.github.com>
2024-02-26 22:07:05 +0100

    Changes: Testing API with an existing temp properties JSON file

--- nodejs_server/index.js
+++ nodejs_server/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('node:fs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello World!');
+})
+
+app.get('/get_properties', (req, res) => {
+  
+    try {
+      const properties = fs.readFileSync('./temp_jsondata/properties_temp.json', 'utf8');
+      res.status(200).send(
+        properties
+      );
+    } catch (error) {
+      res.status(404).send("File not found");
+    }  
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Homelty API is listening on port ${port}`);
 })
