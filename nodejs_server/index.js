cfb69078e6c059aa184424a18694d53eb7c7b4ff
Kirill <42815480+EloTheWizzard@users.noreply.github.com>
2024-02-28 17:48:29 +0100

    Added: MySQL database connection and code that inserts listings into the table

--- nodejs_server/index.js
+++ nodejs_server/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const fs = require('node:fs');
+const fs = require('node:fs'
+);
+const mysql = require('mysql')
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: 'root',
+  database: 'homelty_schema',
+  password: 'DbPassword12345!'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+    console.log("Connected!");
+});
 
 // const propertyTool = require('./project_libs/properties_parser');
 
@@ -9,18 +24,60 @@ app.get('/helloworld', (req, res) => {
   res.status(200).send('Hello World!'.json());
 });
 
+async function postListings(properties)
+{
+  const listingInsertSQL = "INSERT INTO listings (price, numOfBedrooms, numOfBathrooms, location, listingType, listingDescription, listingCondition, area) VALUES ?";
+    const imagesInsertSQL = "INSERT INTO listing_images (photourl, listing_fk) VALUES (?)"
+    const values = [];
+
+    for (const pair in urlsAndIds) {
+      values.push(
+          [
+            properties[property].price,
+            properties[property].numOfBedrooms,
+            properties[property].numOfBathrooms,
+            properties[property].location,
+            properties[property].propertyType,
+            properties[property].description,
+            properties[property].condition,
+            properties[property].area
+          ]
+      );
+    }
+}
+
 app.get('/get_properties', (req, res) => {
-    console.log("get properies request initiated")
+    console.log(`\nGet properies request initiated, property id: ${req.query.id ? req.query.id : "whole list"}\nUser ip: ${req.socket.remoteAddress}\n`)
+    const properties = JSON.parse(fs.readFileSync('./temp_jsondata/properties_temp.json', 'utf8'));
+  
     try {
-      const properties = JSON.parse(fs.readFileSync('./temp_jsondata/properties_temp.json', 'utf8'));
+      // con.query(listingInsertSQL, [values], function (err) {
+      //   if (err) throw err;
+      //   con.end();
+      // });
+
+ 
       //if request contains an id param, responding with a specific property, otherwise the whole list
       req.query.id ? res.status(200).send(properties[req.query.id]) : res.status(200).send(properties); 
     } 
     catch (error) {
-        res.status(404).send(`An error occured while sending a request:\n ${error}`);
+        res.status(404).send(`An error occured while working on a request:\n ${error}`);
     }  
+
+    con.end();
+});
+
+app.get('/images/listings', (req, res) => {
+  console.log(`\nGet listing images request initiated, property id: ${req.query.id ? req.query.id : "whole list"}\nUser ip: ${req.socket.remoteAddress}\n`)
+  try {
+      res.status(200).sendFile(`/images/listings_photos/${req.query.imagepath}`, { root: __dirname })
+  } 
+  catch (error) {
+      res.status(404).send(`An error occured while working on a request:\n ${error}`);
+  }  
 });
 
 app.listen(port, () => {
   console.log(`Homelty API is listening on port ${port}`);
 });
+
