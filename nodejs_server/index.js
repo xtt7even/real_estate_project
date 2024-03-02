be1f1486070deefe6392dfd43236bd0bdc6bf4b3
Kirill <42815480+EloTheWizzard@users.noreply.github.com>
2024-03-02 18:39:51 +0100

    Changes: no more JSON files to fetch data, database data only, retrieving listings and images independantly and merging it.

--- nodejs_server/index.js
+++ nodejs_server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const fs = require('node:fs'
-);
-const mysql = require('mysql')
+const fs = require('node:fs');
+const mysql = require('mysql');
 
-var con = mysql.createConnection({
+app.use('/static', express.static('public'))
+
+var dbConnection = mysql.createPool({
   host: "localhost",
   port: "3306",
   user: 'root',
@@ -13,70 +14,104 @@ var con = mysql.createConnection({
   password: 'DbPassword12345!'
 });
 
-con.connect(function(err) {
+dbConnection.getConnection(function(err) {
   if (err) throw err;
     console.log("Connected!");
 });
 
-// const propertyTool = require('./project_libs/properties_parser');
+// import * as propFetcher from './project_libs/propertiesFetcher.js';
 
-app.get('/helloworld', (req, res) => {
-  res.status(200).send('Hello World!'.json());
-});
+function getDataPromise (sql, connection) {
+  return new Promise(function (resolve, reject) {
+    console.log("Retrieving some data from the DB")
+    connection.query(sql, (error, result) => {
+      if (error) {
+        console.log(error);
+        reject (error);
+      }
+      resolve (result);
+    }); 
+  });
+}
 
-async function postListings(properties)
+
+function imagesSort(images)
 {
-  const listingInsertSQL = "INSERT INTO listings (price, numOfBedrooms, numOfBathrooms, location, listingType, listingDescription, listingCondition, area) VALUES ?";
-    const imagesInsertSQL = "INSERT INTO listing_images (photourl, listing_fk) VALUES (?)"
-    const values = [];
+  for (let i = 0; i < images.length - 1 - i; i++) {
+    for (let j = 0; j < images.length -1 - i; j++) 
+    {
+      if (images[j].listing_fk > images[j+1].listing_fk)
+      {
+        let temp = images[j];
+        images[j] = images[j+1];
+        images[j+1] = temp;
+      }
+    } 
+  }
+  return images;
+}
 
-    for (const pair in urlsAndIds) {
-      values.push(
-          [
-            properties[property].price,
-            properties[property].numOfBedrooms,
-            properties[property].numOfBathrooms,
-            properties[property].location,
-            properties[property].propertyType,
-            properties[property].description,
-            properties[property].condition,
-            properties[property].area
-          ]
-      );
+let sleep = ms => new Promise(r => setTimeout(r, ms));
+let waitFor = async function waitFor(func){
+    while(!func()) await sleep(1000);
+    return func();
+};
+
+async function injectImages(listings, images) 
+{
+  if (await waitFor(() => (listings && images))) {
+    images = imagesSort(images);
+    console.log("Images sorted");
+    console.log(listings.length)
+    for (const listing in listings) {
+      listings[listing].images = [];
+      console.log("Initializing images array in the listing params")
     }
+    for (let image = 0, listing = images[0].listing_fk-1; listing < listings.length, image < images.length; image++) {
+      console.log(images[image].listing_fk-1, listing, images[listing].listing_fk)
+      if (images[image].listing_fk-1 !== listing) listing++;
+      listings[listing].images.push(images[image].photourl);
+      console.log("Some magic with.. IDK WTF IS THIS, BUT IF YOU READ THIS IT SHOULD BE OK")
+    }
+    return listings;
+  }
 }
 
 app.get('/get_properties', (req, res) => {
     console.log(`\nGet properies request initiated, property id: ${req.query.id ? req.query.id : "whole list"}\nUser ip: ${req.socket.remoteAddress}\n`)
-    const properties = JSON.parse(fs.readFileSync('./temp_jsondata/properties_temp.json', 'utf8'));
+    // const properties = JSON.parse(fs.readFileSync('./temp_jsondata/properties_temp.json', 'utf8'));
   
-    try {
-      // con.query(listingInsertSQL, [values], function (err) {
-      //   if (err) throw err;
-      //   con.end();
-      // });
+    let listings = []; 
+    let images = [];
 
- 
-      //if request contains an id param, responding with a specific property, otherwise the whole list
-      req.query.id ? res.status(200).send(properties[req.query.id]) : res.status(200).send(properties); 
-    } 
-    catch (error) {
-        res.status(404).send(`An error occured while working on a request:\n ${error}`);
-    }  
+    getDataPromise("SELECT * FROM listings", dbConnection)
+    .then(
+      function (result) {
+        listings = result;
+        return getDataPromise("SELECT * FROM listing_images", dbConnection)
+      }
+    )
+    .then (
+      function (result) {
+        images = result;
+        // console.log(listings.length)
+        return injectImages(listings, images);
+      }
+    )
+    .then ((listings) => {
+        try {
+          console.log(listings)
+          res.status(200).send(listings);
+        } 
+        catch (error) {
+          res.status(404).send(`An error occured while working on a request:\n ${error}\n(not ok.)`);
+        }  
+      }
+    )
+    
 
-    con.end();
 });
-
-app.get('/images/listings', (req, res) => {
-  console.log(`\nGet listing images request initiated, property id: ${req.query.id ? req.query.id : "whole list"}\nUser ip: ${req.socket.remoteAddress}\n`)
-  try {
-      res.status(200).sendFile(`/images/listings_photos/${req.query.imagepath}`, { root: __dirname })
-  } 
-  catch (error) {
-      res.status(404).send(`An error occured while working on a request:\n ${error}`);
-  }  
-});
-
+  
 app.listen(port, () => {
   console.log(`Homelty API is listening on port ${port}`);
 });
