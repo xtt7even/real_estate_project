f328e40e088087f5ffc835317d41164a26794ceb
Kirill <42815480+EloTheWizzard@users.noreply.github.com>
2024-03-08 13:36:41 +0100

    Fixes: Homepage, fixed desktop responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,41 +6,43 @@ import logo_white from '../../images/logo3_white.png'
 function Footer()
 {
     return (
-        <div className="Footer--container">
-            <div className="Footbar--section">
-                <div className="Footbar--branding">
-                    <img id="Footbar--logo" src={logo_white}></img>
-                    <h3>Homelty</h3>
+        <div id="Footer--container">
+            <div id="Footer--cropper">
+                <div className="Footbar--section">
+                    <div className="Footbar--branding">
+                        <img id="Footbar--logo" src={logo_white}></img>
+                        <h3>Homelty</h3>
+                    </div>
+
+                    <div className="Footbar--address">
+                        <span>123 Main Street, City, State, ZIP</span> 
+                        <span>Phone: (123) 456-7890</span>
+                        <span>Email: info@homelty.com</span>
+                    </div>
+
+                    <div className="Footbar--copyright">
+                        © 2024 Homelty. All rights reserved.
+                    </div>
                 </div>
 
-                <div className="Footbar--address">
-                    <span>123 Main Street, City, State, ZIP</span> 
-                    <span>Phone: (123) 456-7890</span>
-                    <span>Email: info@homelty.com</span>
+                <div className="Footbar--section">
+                    <h3>Navigation</h3>
+                    <div className="Footbar--links">
+                        <Link className="Footbar--link" to="/">Home</Link>
+                        <Link className="Footbar--link" to="/listings">Listings</Link>
+                        <Link className="Footbar--link" to="/aboutus">About Us</Link>
+                        <Link className="Footbar--link" to="/contact">Contact</Link>
+                    </div>
                 </div>
 
-                <div className="Footbar--copyright">
-                    © 2024 Homelty. All rights reserved.
-                </div>
-            </div>
-
-            <div className="Footbar--section">
-                <h3>Navigation</h3>
-                <div className="Footbar--links">
-                    <Link className="Footbar--link" to="/">Home</Link>
-                    <Link className="Footbar--link" to="/listings">Listings</Link>
-                    <Link className="Footbar--link" to="/aboutus">About Us</Link>
-                    <Link className="Footbar--link" to="/contact">Contact</Link>
-                </div>
-            </div>
-
-            <div className="Footbar--section">
-                <h3>Social Media</h3>
-                <div className="Footbar--links">
-                    <a href="https://here_is_your_url.com" className="Footbar--link">Instagram</a>
-                    <a href="https://here_is_your_url.com" className="Footbar--link">Facebook</a>
-                    <a href="https://here_is_your_url.com" className="Footbar--link">Twitter</a>
-                    <a href="https://here_is_your_url.com" className="Footbar--link">LinkedIn</a>
+                <div className="Footbar--section">
+                    <h3>Social Media</h3>
+                    <div className="Footbar--links">
+                        <a href="https://here_is_your_url.com" className="Footbar--link">Instagram</a>
+                        <a href="https://here_is_your_url.com" className="Footbar--link">Facebook</a>
+                        <a href="https://here_is_your_url.com" className="Footbar--link">Twitter</a>
+                        <a href="https://here_is_your_url.com" className="Footbar--link">LinkedIn</a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/WhyHomelty/WhyHomelty.jsx
+++ src/components/WhyHomelty/WhyHomelty.jsx
@@ -7,23 +7,25 @@ function WhyHomelty()
 {
     return(
         <div className="Whyhomelty--container">
-            <div className="Benefits--list">
-                <h2>Why Homelty?</h2>
-                <div className="Benefit--container">
-                    <img src={checkMark} className="Checkmark--img"></img>
-                    <p><b>Experience</b>: Benefit from our years of experience in the real estate industry, ensuring you receive expert guidance and support throughout your journey.</p>
+            <div id="Whyhomelty--cropper">
+                <div className="Benefits--list">
+                    <h2>Why Homelty?</h2>
+                    <div className="Benefit--container">
+                        <img src={checkMark} className="Checkmark--img"></img>
+                        <p><b>Experience</b>: Benefit from our years of experience in the real estate industry, ensuring you receive expert guidance and support throughout your journey.</p>
+                    </div>
+                    <div className="Benefit--container">
+                        <img src={checkMark} className="Checkmark--img"></img>
+                        <p><b>Local Expertise</b>: Rely on our deep understanding of the local market and neighborhoods, giving you an insider's perspective and helping you make informed decisions.</p>
+                    </div>
+                    <div className="Benefit--container">
+                        <img src={checkMark} className="Checkmark--img"></img>
+                        <p><b>Personalized Service</b>: Enjoy personalized attention and tailored solutions designed to meet your unique needs and preferences, making your real estate experience seamless and stress-free.</p>
+                    </div>
                 </div>
-                <div className="Benefit--container">
-                    <img src={checkMark} className="Checkmark--img"></img>
-                    <p><b>Local Expertise</b>: Rely on our deep understanding of the local market and neighborhoods, giving you an insider's perspective and helping you make informed decisions.</p>
+                <div className="Benefits--photo--container">
+                    <img className="Benefits--photo" src={benefitsPhoto}></img>
                 </div>
-                <div className="Benefit--container">
-                    <img src={checkMark} className="Checkmark--img"></img>
-                    <p><b>Personalized Service</b>: Enjoy personalized attention and tailored solutions designed to meet your unique needs and preferences, making your real estate experience seamless and stress-free.</p>
-                </div>
-            </div>
-            <div className="Benefits--photo--container">
-                <img className="Benefits--photo" src={benefitsPhoto}></img>
             </div>
         </div>
     );
